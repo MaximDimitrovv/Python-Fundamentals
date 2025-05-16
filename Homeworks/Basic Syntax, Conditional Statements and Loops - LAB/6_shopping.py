abaf3c6b61f071de9fef89bda74b8e834a9a38d6
Maksim 2 - DELL <maksimdimitrov.dekaeksi@gmail.com>
2025-05-16 17:52:49 +0300

    16.05.25

---
+++ Homeworks/Basic Syntax, Conditional Statements and Loops - LAB/6_shopping.py
@@ -0,0 +1,16 @@
+budget = int(input())
+
+total_price = 0
+
+while budget > total_price:
+    command = input()
+
+    if command == "End":
+        print("You bought everything needed.")
+        break
+
+    price = int(command)
+    total_price += price
+
+else:
+    print("You went in overdraft!")
